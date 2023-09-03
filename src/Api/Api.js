a335d23d6c788690210906b185ac4586ac0b5acc
Пользователь <ryabchenkora72@gmail.com>
2023-09-03 16:28:22 +0500

    server2

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,8 +1,10 @@
 import axios from "axios";
+// import { dataSlider } from "../Data/Data"
 
 export const LoadSlider = async () =>
   axios
-    .get("http://localhost:3001/dataSlider")
+    // .get("http://localhost:3001/dataSlider")
+    .get("../Data/Data")
     .then((response) => response.data)
 
     .catch((error) => {
--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import { LoadSlider } from './Api/Api';
+// import { LoadSlider } from './Api/Api';
 import { Slider } from './Components/Slider/Slider';
 import { Loader } from './Components/Loader/Loader';
+import { dataSlider } from './Data/Data'
 
 function App() {
-  const [sliders, setSliders] = useState([])
+  const [sliders, setSliders] = useState(dataSlider)
   const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     const initSlider = async () => {
-      const slider = await LoadSlider();
+      const slider = await dataSlider();
       setSliders(slider);
     };
 
